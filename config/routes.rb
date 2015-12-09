bdbdac0654502aa5afa41dcf178ce0a24be27b06
Sia <siakaramalegos@gmail.com>
2015-12-09 08:50:50 -0600

    DRYed up routes and defaulted to JSON response format

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-  namespace :v1 do
-    get 'products/index'
-    end
-  end
-
-  namespace :api do
-  namespace :v1 do
-    get 'products/show'
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :products, only: [:index, :show]
     end
   end
 
